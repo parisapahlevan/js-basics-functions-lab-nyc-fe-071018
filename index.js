83c642b1b9b71183f885878c2572823a1b47aacf
parisa pahlevan <paris.pahlevan@gmail.com>
2018-08-21 04:09:10 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -21,11 +21,31 @@ function distanceTravelledInFeet(startingBlock,endingBlock){
 }
 
 function calculatesFarePrice(){
- switch  calculatesFarePrice
- case
+ switch  (calculatesFarePrice
+ case 400: 
+   fee = "free";
+   break;
  block 
 }
-
+switch (new Date().getDay()) {
+    case 0:
+        day = "Sunday";
+        break;
+    case 1:
+        day = "Monday";
+        break;
+    case 2:
+        day = "Tuesday";
+        break;
+    case 3:
+        day = "Wednesday";
+        break;
+    case 4:
+        day = "Thursday";
+        break;
+    case 5:
+        day = "Friday";
+        break;
 
 
 
