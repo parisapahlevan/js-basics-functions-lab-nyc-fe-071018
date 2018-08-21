9001613f2a72e70c4dc10ec53b68330d5758a165
parisa pahlevan <paris.pahlevan@gmail.com>
2018-08-21 04:30:09 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -22,6 +22,15 @@ function distanceTravelledInFeet(startingBlock,endingBlock){
 
 function calculatesFarePrice(startingBlock,endingBlock){
   let dInf  = distanceTravelledInFeet(startingBlock, endingBlock)
+  let distanceToPayForInFeet = dInf - 400
+  if(distanceToPayForInFeet<2000){
+    return distanceToPayForInFeet*2
+  }else if (distanceToPayForInFeet>2000 && 
+            distanceToPayForInFeet<2500){
+    return 'flat fare'
+  }else{
+    
+  }
 }
 
  
