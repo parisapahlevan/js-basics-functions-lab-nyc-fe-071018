994e5886137c7478c3706e07f05ab69da4fa6e05
parisa pahlevan <paris.pahlevan@gmail.com>
2018-08-21 02:43:47 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,3 +14,18 @@ function distanceFromHqInBlocks(x){
 function distanceTravelledInFeet(x) {
   return x=264;
 }
+
+
+describe('distanceFromHqInFeet()', function() {
+    it('returns a distance in feet', function() {
+      expect(distanceFromHqInFeet(43)).to.equal(264);
+    });
+
+    it('returns a distance in feet', function() {
+      expect(distanceFromHqInFeet(50)).to.equal(2112);
+    });
+
+    it('calculates distances below 42nd street', function() {
+      expect(distanceFromHqInFeet(34)).to.equal(2112);
+    });
+  });
